152ecd93a778fdd7b50d413a3d6ac24b6a7f5330
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-18 22:59:25 -0500

    retype original map.html

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var marker = new google.maps.Marker({
+position: new google.maps.LatLng(41.902748, -87.720222),
+map: map,
+});
+
+
+const chicagoCoords = [
+  { lat: 41.0793, lng: -85.1394 },
+  { lat: 43.5460, lng: -96.7313 },
+];
+
+
+var map;
+function initMap (){
+map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 8,
+  Center: {lat: 41.902748, lng: -87.720222},
+});
