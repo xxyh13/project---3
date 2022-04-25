a56f9336f8f2120a266bcf099031b8421f8fb9e6
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-24 21:01:24 -0500

    changed map.html

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-function initMap() {
-  const chicago = { lat: 41.8781, lng: -87.6298 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 6,
-    center: chicago,
-    mapTypeId: "terrain",
-  });
+
   const chicagoCoords = [
       { lat: 41.0793, lng: -85.1394 },
       { lat: 43.5460, lng: -96.7313 },
