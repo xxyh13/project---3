566aa56c02062397754d6ac0a86009b362b07da1
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-24 21:48:26 -0500

    edit map

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
-function initMap() {
-  const chicago = { lat: 41.8781, lng: -87.6298 };
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.8781, lng: -87.6298 },
-    zoom: 8,
-  });
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
 
-  const marker = new google.maps.Marker({
-    position: chicago,
-    map: map,
-    icon: icon,
-  });
-
-  const chicagoCoords = [
-    { lat: 41.0793, lng: -85.1394 },
-    { lat: 43.5460, lng: -96.7313 },
-  ];
-
-  google.maps.event.addDomListener(window, 'load', initMap);
+  window.initMap = initMap;
