c3757bfb40d5399f1334a048edaea3ba65ba4208
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-24 22:30:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
   function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 41.8781, lng: -87.6298 },
+    const chicago = { lat: 41.8781, lng: -87.6298 };
+     const map = new google.maps.Map(document.getElementById("map"), {
+      center: chicago,
       zoom: 8,
+      mapTypeId: "terrain",
     });
   }
   const marker = new google.maps.Marker({
@@ -9,15 +11,15 @@
     map: map,
     icon: icon,
   });
-  
-var icon = {
-  url: "https://media.istockphoto.com/photos/dramatic-sunset-downtown-chicago-picture-id1204331594?b=1&k=20&m=1204331594&s=170667a&w=0&h=DYmAnf_omKcY0we0Q_Vd5hrt0CEBCxTsIkhwKeC-TpE=",
-  scaledSize: new google.maps.Size(100,100),
-};
+
+  var icon = {
+    url: "https://media.istockphoto.com/photos/dramatic-sunset-downtown-chicago-picture-id1204331594?b=1&k=20&m=1204331594&s=170667a&w=0&h=DYmAnf_omKcY0we0Q_Vd5hrt0CEBCxTsIkhwKeC-TpE=",
+    scaledSize: new google.maps.Size(100,100),
+  };
 
   const chicagoCoords = [
     { lat: 41.0793, lng: -85.1394 },
     { lat: 43.5460, lng: -96.7313 },
   ];
 
-  window.initMap = initMap;
+window.initMap = initMap;
