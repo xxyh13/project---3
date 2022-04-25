cfc98b2924d65273f9090f1a0ecdba6a8335216a
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-25 11:56:27 -0500

    added js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function initMap() {
   const chicago = { lat: 41.8781, lng: -87.6298 };
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.8781, lng: -87.6298 },
+      center: chicago,
       zoom: 8,
       });
 
 const marker = new google.maps.Marker({
     position: chicago,
     map: map,
-    icon: icon,
+    animation: google.maps.Animation.BOUNCE
       });
 
 const chicagoCoords = [
