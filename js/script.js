23c7a20d9aff7ad34c423ca11b58fffd0b2e91f5
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-24 21:19:13 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 41.8781, lng: -87.6298 },
     zoom: 8,
   });
 
