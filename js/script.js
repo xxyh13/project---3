e26252f8c56ccb7bd9ecf0148209b2d3b4864d82
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-24 23:07:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
 function initMap() {
-    const myLatLng = { lat: 41.8781, lng: -87.6298 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: myLatLng
-      zoom: 4,
-    });
-    
-var myLatLng = new google.maps.LatLng(41.8781,-87.6298);
-var mapOptions = {
-  center: myLatLng
-  zoom: 4,
-}
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  const chicago = { lat: 41.8781, lng: -87.6298 };
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 41.8781, lng: -87.6298 },
+      zoom: 8,
+      });
 
-var marker = new google.maps.marker({
-  position: myLatLng,
-  title: "Chicago, IL"
- });
-}
-window.initMap = initMap;
-marker.setMap(map);
+  const marker = new google.maps.Marker({
+    position: chicago,
+    map: map,
+    icon: icon,
+      });
+
+const chicagoCoords = [
+    { lat: 41.0793, lng: -85.1394 },
+    { lat: 43.5460, lng: -96.7313 },
+      ];
+google.maps.event.addDomListener(window, 'load', initMap);
