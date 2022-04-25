1365cbcfe8826ff255b16d8cb14bb94f662083d0
Alma Hernandez <ahernandez16@hawk.iit.edu>
2022-04-24 20:55:50 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ function initMap() {
     center: chicago,
     mapTypeId: "terrain",
   });
+  const chicagoCoords = [
+      { lat: 41.0793, lng: -85.1394 },
+      { lat: 43.5460, lng: -96.7313 },
+    ];
